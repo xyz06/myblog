543b0a0d698d4de6277a052ee072b76a0ddd21e2
xyz06 <1056156342@qq.com>
2020-08-07 17:11:04 +0800

    add js,css

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,6 +3,12 @@ import settings
 from exts import *
 from apps.user.view import user_bp
 from apps.article.view import article_bp
+config = {
+    'CACHE_TYPE' : 'REDIS',
+    'CACHE_REDIS_HOST':'192.168.52.100',
+    'CACHE_REDIS_PORT' : 6379
+}
+
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
@@ -13,6 +19,8 @@ def create_app():
     app.register_blueprint(article_bp)
     #数据库
     db.init_app(app)
+
+    cahe.init_app(app=app,config=config)
 
     bs.init_app(app=app)
 
--- exts/__init__.py
+++ exts/__init__.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_caching import Cache
 
 db = SQLAlchemy()
 bs = Bootstrap()
+cahe = Cache()
